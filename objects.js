5f48f05bf62e32b6ed97f4d6a8f9c30b2be5c24b
Liam Roberts <liam_r_123@hotmail.co.uk>
2018-11-12 13:19:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist.Slowdive = "Alison"
   playlist["My Bloody Valentine"] = "Sometimes";
 }
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.Slowdive;
+}
