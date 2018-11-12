e518bc06dffc354057a7300939d5642507a5724c
Liam Roberts <liam_r_123@hotmail.co.uk>
2018-11-12 13:14:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,7 @@
 var playlist = { jimiHendrix: "Purple Haze"};
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = songTitle;
+  playlist.Slowdive = "Alison"
+  playlist.[My Bloody Valentine] = "Sometimes";
+  playlist.[Phil Ochs] = "Here's to the State of Mississippi";
 }
